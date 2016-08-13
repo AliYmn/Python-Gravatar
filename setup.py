f34af0fdee909b4aa357207eec6f98aa3d47ecea
Ali Yaman <aliymn.db@gmail.com>
2016-08-14 01:06:08 +0300

    Test edildi.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Python-Gravatar',
+    version='1.0',
+    packages=[''],
+    url='https://github.com/AliYmn/Python-Gravatar',
+    license='MIT License',
+    author='aliymn',
+    author_email='aliymn.db@gmail.com',
+    description='Python gravatar profile package'
+)
